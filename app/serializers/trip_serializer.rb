7071b11bf865a796596c20ef260b7de126a01564
Muhidin <muhidinhukic1@gmail.com>
2021-02-22 22:02:04 -0600

    cleanup

--- app/serializers/trip_serializer.rb
+++ app/serializers/trip_serializer.rb
@@ -2,8 +2,9 @@ class TripSerializer < ActiveModel::Serializer
     include Rails.application.routes.url_helpers
     attributes :id, :title, :destination_name, :start, :end, :note, :image, :latitude_delta, :longitude_delta, :longitude, :latitude, :checkpoints, :user_id
                            
-    # belongs_to :user
+    belongs_to :user
     has_many :checkpoints
+
 
     def image
       url_for(object.image) if object.image.attached?
